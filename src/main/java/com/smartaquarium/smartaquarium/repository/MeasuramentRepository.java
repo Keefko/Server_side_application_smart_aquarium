eee8153690f392f5a441e6594ba33f23f0d5fe39
Dan Kohout <kohooutdan@gmail.com>
2020-05-05 19:50:21 +0200

    another fix

--- src/main/java/com/smartaquarium/smartaquarium/repository/MeasuramentRepository.java
+++ src/main/java/com/smartaquarium/smartaquarium/repository/MeasuramentRepository.java
@@ -2,8 +2,10 @@ package com.smartaquarium.smartaquarium.repository;
 
 import com.smartaquarium.smartaquarium.entity.Measurament;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -15,6 +17,8 @@ public interface MeasuramentRepository extends JpaRepository<Measurament, Intege
     @Query("SELECT m FROM Measurament  m WHERE m.aquariumId = ?1 AND  m.createTime BETWEEN ?2 AND ?3 ")
     List<Measurament> getAllMeasuramentByDate(Integer aquariumId, Timestamp from, Timestamp to);
 
+    @Transactional
+    @Modifying
     @Query("DELETE FROM Measurament m WHERE m.aquariumId = ?1")
     void deleteAllByAquariumId(Integer aquariumId);
 

3775772292172a7d4b73b7fc792376eb970ac825
Dan Kohout <kohooutdan@gmail.com>
2020-05-24 19:37:20 +0200

    some fixes

--- src/main/java/com/smartaquarium/smartaquarium/service/handling/MeasuramentProccess.java
+++ src/main/java/com/smartaquarium/smartaquarium/service/handling/MeasuramentProccess.java
@@ -1,10 +1,12 @@
 package com.smartaquarium.smartaquarium.service.handling;
 
 import com.smartaquarium.smartaquarium.entity.AquariumSettings;
+import com.smartaquarium.smartaquarium.entity.Connection;
 import com.smartaquarium.smartaquarium.entity.Measurament;
 import com.smartaquarium.smartaquarium.entity.Notification;
 import com.smartaquarium.smartaquarium.repository.NotificationRepository;
 import com.smartaquarium.smartaquarium.service.AquariumSettingsService;
+import com.smartaquarium.smartaquarium.service.ConnectionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
@@ -14,19 +16,42 @@ public class MeasuramentProccess {
 
     private NotificationRepository notificationRepository;
     private AquariumSettingsService aquariumSettingsService;
+    private ConnectionService connectionService;
 
     @Autowired
-    public MeasuramentProccess(NotificationRepository notificationRepository, AquariumSettingsService aquariumSettingsService) {
+    public MeasuramentProccess(NotificationRepository notificationRepository, AquariumSettingsService aquariumSettingsService, ConnectionService connectionService) {
         this.notificationRepository = notificationRepository;
         this.aquariumSettingsService = aquariumSettingsService;
+        this.connectionService = connectionService;
     }
 
     @NonNull
     public void measuramentControl(Measurament measurament){
         AquariumSettings aquariumSettings = aquariumSettingsService.getSettingByAquariumId(measurament.getAquariumId());
+        Connection connection = connectionService.getByAquariumId(measurament.getAquariumId());
 
         if(aquariumSettings ==  null){
             throw new RuntimeException("Akvárium nemá nastavenia");
+        }
+
+        if(connection == null) {
+            Boolean phSenzor = false;
+            Boolean orpSenzor = false;
+            Boolean thermometer = false;
+
+            if(measurament.getPh() != 0){
+                phSenzor = true;
+            }
+
+            if(measurament.getOrp() != 0){
+                orpSenzor = true;
+            }
+
+            if(measurament.getTemperature() != 0.0){
+                thermometer = true;
+            }
+            Connection newCon = new Connection(measurament.getAquariumId(),phSenzor,orpSenzor,thermometer,false, false, false);
+            connectionService.add(newCon);
         }
 
         int phWarning = phControl(measurament.getPh(),aquariumSettings.getPh());

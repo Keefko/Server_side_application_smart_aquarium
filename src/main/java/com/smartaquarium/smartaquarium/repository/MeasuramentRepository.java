cf428a2db73d4171df4e2c5061dcdb0171598722
Dan Kohout <kohooutdan@gmail.com>
2020-06-14 15:36:11 +0200

    fixes

--- src/main/java/com/smartaquarium/smartaquarium/repository/MeasuramentRepository.java
+++ src/main/java/com/smartaquarium/smartaquarium/repository/MeasuramentRepository.java
@@ -43,12 +43,18 @@ public interface MeasuramentRepository extends JpaRepository<Measurament, Intege
     @Query("SELECT AVG(m.orp), m.createTime FROM Measurament m WHERE m.aquariumId = ?1 AND m.createTime BETWEEN ?2 AND ?3 group by date(create_time)")
     List<Object[]> getOrpAvgD(Integer aquariumdId,Timestamp from, Timestamp to);
 
-    @Query(value = "SELECT CONCAT(WEEK(m.createTime) - WEEK(DATE_SUB(m.createTime, INTERVAL DAYOFMONTH(m.createTime) - 1 DAY ))+1,MONTH(m.createTime), YEAR(m.createTime)), ROUND(AVG(m.temperature),2)FROM Measurament m WHERE m.aquarium_id = ?1 AND m.createTime BETWEEN ?2 AND ?3 GROUP BY WEEK(m.createTime)", nativeQuery = true)
+    @Modifying
+    @Transactional
+    @Query(value = "SELECT CONCAT(WEEK(m.createTime) - WEEK(DATE_SUB(m.createTime, INTERVAL DAYOFMONTH(m.createTime) - 1 DAY ))+1,MONTH(m.createTime), YEAR(m.createTime)), ROUND(AVG(m.temperature),2) FROM Measurament m WHERE m.aquarium_id = ?1 AND m.createTime BETWEEN ?2 AND ?3 GROUP BY WEEK(m.createTime)", nativeQuery = true)
     List<Object[]> getThermoAvgW(Integer aquariumdId,Timestamp from, Timestamp to);
 
+    @Modifying
+    @Transactional
     @Query(value = "SELECT CONCAT(WEEK(m.createTime) - WEEK(DATE_SUB(m.createTime, INTERVAL DAYOFMONTH(m.createTime) - 1 DAY ))+1,MONTH(m.createTime), YEAR(m.createTime)), ROUND(AVG(m.orp),2) FROM Measurament m WHERE m.aquarium_id = ?1 AND m.createTime BETWEEN ?2 AND ?3 GROUP BY WEEK(m.createTime)",nativeQuery = true)
     List<Object[]> getOrpAvgW(Integer aquariumdId,Timestamp from, Timestamp to);
 
+    @Modifying
+    @Transactional
     @Query(value = "SELECT CONCAT(WEEK(m.createTime) - WEEK(DATE_SUB(m.createTime, INTERVAL DAYOFMONTH(m.createTime) - 1 DAY ))+1,MONTH(m.createTime),YEAR(m.createTime)), ROUND(AVG(m.ph),2) FROM Measurament m WHERE m.aquarium_id = ?1 AND m.createTime BETWEEN ?2 AND ?3 GROUP BY WEEK(m.createTime)", nativeQuery = true)
     List<Object[]> getPhAvgW(Integer aquariumdId, Timestamp from, Timestamp to);
 

2bc0b532948873fa8bac3bfbda904e23a9790806
Dan Kohout <kohooutdan@gmail.com>
2020-07-01 20:51:14 +0200

    f

--- src/main/java/com/smartaquarium/smartaquarium/SmartaquariumApplication.java
+++ src/main/java/com/smartaquarium/smartaquarium/SmartaquariumApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
-@EnableScheduling
+
 public class SmartaquariumApplication {
 
 	public static void main(String[] args) {
